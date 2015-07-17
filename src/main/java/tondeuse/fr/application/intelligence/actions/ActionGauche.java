0acce45d2d6d1371fb68cfa64b6bbe2954dc9302
mathieuherbert <math.herbert@gmail.com>
2015-07-17 16:20:39 +0200

    Test de l'action gauche

---
+++ src/main/java/tondeuse/fr/application/intelligence/actions/ActionGauche.java
@@ -0,0 +1,19 @@
+package tondeuse.fr.application.intelligence.actions;
+
+import tondeuse.fr.application.model.Position;
+import tondeuse.fr.application.model.Tondeuse;
+
+public class ActionGauche extends AbstractAction {
+
+	public ActionGauche(Position coinSuperieurDroit) {
+		super(coinSuperieurDroit);
+	}
+
+	@Override
+	public void execute(Tondeuse tondeuse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
