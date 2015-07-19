f6c683cf6fd032310af519fa949eba5c8d6114e2
mathieuherbert <math.herbert@gmail.com>
2015-07-19 20:16:08 +0200

    Mise en place du Launcher

--- src/main/java/tondeuse/fr/application/Launcher.java
+++ src/main/java/tondeuse/fr/application/Launcher.java
@@ -1,9 +1,25 @@
 package tondeuse.fr.application;
 
+import tondeuse.fr.application.exceptions.ParsingException;
+import tondeuse.fr.application.fichiers.GestionFichier;
+import tondeuse.fr.application.intelligence.PassageTondeuses;
+import tondeuse.fr.application.model.Plateau;
+
+/**
+ * Classe de lancement de l'application
+ * @author Mathieu
+ *
+ */
 public class Launcher {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParsingException {
+		if(args.length != 1){
+			throw new IllegalArgumentException("Le chemin vers le fichier doit etre donne en entree");
+		}
+		
+		Plateau plateau = GestionFichier.chargerFichier(args[0]);
+		PassageTondeuses passageTondeuses = new PassageTondeuses(plateau);
+		passageTondeuses.passerTondeuses();
 		
 	}
 
